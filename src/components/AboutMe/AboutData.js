6b0ecef8a4904009effab255e396d8d4defedbf5
GeGonzalez10 <geneandmafer@gmail.com>
2021-10-29 06:46:18 -0400

    Carousel

--- src/components/AboutMe/AboutData.js
+++ src/components/AboutMe/AboutData.js
@@ -5,7 +5,7 @@ export const AboutDataObj = {
     topLine: 'Desarrolladora Web en construcción🤓',
     heading: 'Sobre mí',
     description: 'Soy una estudiante de Ingeniería Electrónica en la Universidade Simón Bolívar, Venezuela. Estoy interesada en impulsar mi crecimiento profesional con la programación. Actualmente, estoy desarrollando mis conocimientos de forma autodidacta en la rama de Desarrollo web, he estado usando principalmente HTML, CSS y JavaScript, así como también NodeJs, ReactJs y librerías como Bootstrap y Materialize.',
-    imgStart: false,
+    imgStart: true,
     img: require('../../images/aboutme.svg'),
     alt: 'AboutMe',
     darkText: true,
